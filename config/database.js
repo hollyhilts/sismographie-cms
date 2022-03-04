df369714fc16c9b57bbf3e621a0c7501de1a185f
Holly Hilts <hollyhilts@Hollys-MacBook-Pro.local>
2022-03-03 20:00:13 -0500

    updated database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,34 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {
+        ssl: false,
+      },
     },
-    useNullAsDefault: true,
   },
 });
