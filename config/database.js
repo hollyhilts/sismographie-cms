2f123a436326c09976582793e2d468c8cc5ac98b
Holly Hilts <hollyhilts@Hollys-MacBook-Pro.local>
2022-03-03 20:15:26 -0500

    put database.js back to the old -- going to try logging in with different users

--- config/database.js
+++ config/database.js
@@ -1,34 +1,15 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const path = require("path");
 
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-      },
-      options: {
-        ssl: false,
-      },
+  connection: {
+    client: "sqlite",
+    connection: {
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
