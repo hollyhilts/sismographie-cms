9b0a19851f6f6fd742eaf55d1e8b9d5255833291
Holly Hilts <hollyhilts@Hollys-MacBook-Pro.local>
2022-03-06 09:15:15 -0500

    deleted empty ballado content type and added proper database url in env file

--- src/api/ballado/routes/ballado.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * ballado router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::ballado.ballado');
--- src/api/ballado/services/ballado.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * ballado service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::ballado.ballado');
