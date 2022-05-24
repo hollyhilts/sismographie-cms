e8ca1136d4eb4126672cead96ec6c11841097bcd
Holly Hilts <hollyhilts@Hollys-MacBook-Pro.local>
2022-05-24 12:17:40 -0400

    added final adjustments and additions for locales and additional text

---
+++ src/api/projects-about-text/services/projects-about-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * projects-about-text service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::projects-about-text.projects-about-text');
