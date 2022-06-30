801ec6553e787515c06a3cc3f2647f87ce6be8af
Holly Hilts <hollyhilts@Hollys-MacBook-Pro.local>
2022-06-30 17:23:06 -0400

    added two more sections: credits and sponsor logos

---
+++ src/api/sponsor-logo/controllers/sponsor-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  sponsor-logo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sponsor-logo.sponsor-logo');
---
+++ src/api/sponsor-logo/services/sponsor-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sponsor-logo service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sponsor-logo.sponsor-logo');
